10117afead3d0936fe4e533e9b874cdfd0c96fef
melkie <melkieyilk@gmail.com>
2024-12-22 22:51:34 +0300

    Make a cart item design and test with some hard coded

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,13 +2,13 @@ import { Fragment } from "react";
 import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.png";
 import classes from "./Header.module.css";
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1 className="text-3xl font-bold">ReactMeals</h1>
 
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="meals Image" />
