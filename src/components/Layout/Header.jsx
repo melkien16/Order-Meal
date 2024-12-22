3573268d744ce103c8d4cd28f2e3d7db5bee4e1a
melkie <melkieyilk@gmail.com>
2024-12-22 15:00:06 +0300

    Create another jsx file for the cart button and add in to the header

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.png";
 import classes from "./Header.module.css";
 const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <h1 className="text-3xl font-bold">ReactMeals</h1>
+
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="meals Image" />
