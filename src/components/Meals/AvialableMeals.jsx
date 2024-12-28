5e62316386ae7e1c8dafee32ca9762c09b82801c
melkie <melkieyilk@gmail.com>
2024-12-28 21:48:10 +0300

    Enhance the UX by adding loading and error message

--- src/components/Meals/AvialableMeals.jsx
+++ src/components/Meals/AvialableMeals.jsx
@@ -1,17 +1,24 @@
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from "react";
 
-import Card from '../UI/Card';
-import MealItem from './MealItem/MealItem';
-import classes from './AvailableMeals.module.css';
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
 
 const AvialableMeals = () => {
-
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch('https://meals-f8fa9-default-rtdb.firebaseio.com/meals.json');
-      
+      const response = await fetch(
+        "https://meals-f8fa9-default-rtdb.firebaseio.com/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -21,21 +28,41 @@ const AvialableMeals = () => {
           id: key,
           name: responseData[key].name,
           description: responseData[key].description,
-          price: responseData[key].price
+          price: responseData[key].price,
         });
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-    fetchMeals();
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   return (
     <section className={classes.meals}>
       <Card>
         <ul>
-          {meals.map(meal => (            
+          {meals.map((meal) => (
             <MealItem
               key={meal.id}
               id={meal.id}
@@ -47,7 +74,7 @@ const AvialableMeals = () => {
         </ul>
       </Card>
     </section>
-  )
-}
+  );
+};
 
-export default AvialableMeals
+export default AvialableMeals;
